f8c5bd40c96344eacdeba762081e528802749f49
mayra@mumuki.org <mayra@mumuki.org>
2021-03-19 17:42:53 +0000

    Mumuki Export on 2021-03-19 17:42:53 +0000

--- 00002_Sumar/test..js
+++ 00002_Sumar/test..js
@@ -4,6 +4,6 @@ it("Si hacemos sumar([[4, 5],[2, 7, 1],[8, 10],]) debería retornar 37", functio
 })
 
 it("Si hacemos sumar([[19, 5, 22],[21, 3, 24, 4],] debería retornar 98", function() {
-  let resultado = sumar([[19, 5, 22],[21, 3, 24, 4],] 
+  let resultado = sumar([[19, 5, 22],[21, 3, 24, 4],]) 
   assert.equal(resultado, 98,`La suma debería retornar 98 pero fue ${resultado}`);
 })
