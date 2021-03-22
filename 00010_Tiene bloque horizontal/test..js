bf22a1ab1a2dd9f47f6625d5b86d8288038aed91
mayra@mumuki.org <mayra@mumuki.org>
2021-03-22 14:46:05 +0000

    Mumuki Export on 2021-03-22 14:46:05 +0000

--- 00010_Tiene bloque horizontal/test..js
+++ 00010_Tiene bloque horizontal/test..js
@@ -1,8 +1,8 @@
-it("Si hacemos tieneBloqueHorizontal([
+it(`Si hacemos tieneBloqueHorizontal([
   [1, 2, 3],
   [1, 2, 3],
   [1, 2, 3],
-]) debería retornar false", function() {
+]) debería retornar false`, function() {
   let resultado = tieneBloqueHorizontal([
   [1, 2, 3],
   [1, 2, 3],
@@ -11,11 +11,11 @@ it("Si hacemos tieneBloqueHorizontal([
   assert.equal(resultado, false,`tieneBloqueHorizontal debería retornar false pero fue ${resultado}`);
 })
 
-it("Si hacemos tieneBloqueHorizontal([
+it(`Si hacemos tieneBloqueHorizontal([
   [1, 2, 3, 4],
   [1, 2, 2, 2],
   [1, 2, 4, 5],
-]) debería retornar true", function() {
+]) debería retornar true`, function() {
   let resultado = tieneBloqueHorizontal([
   [1, 2, 3, 4],
   [1, 2, 2, 2],
