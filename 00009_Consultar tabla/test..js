4031aa0f16b0b01fbf246dd9a50c3f732ad0fa4d
mayra@mumuki.org <mayra@mumuki.org>
2021-03-29 20:27:21 +0000

    Mumuki Export on 2021-03-29 20:27:21 +0000

--- 00009_Consultar tabla/test..js
+++ 00009_Consultar tabla/test..js
@@ -1,5 +1,17 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it(`Si hacemos consultarTabla(2, 'nombre', tabla) debería retornar 'Grace'`, function() {
+  let resultado = consultarTabla(2, 'nombre', [
+  ['id', 'nombre', 'edad', 'email'],
+  [1, 'Ada', 33, 'ada@gmail.com'],
+  [2, 'Grace', 45, 'grace@gmail.com'],
+])
+  assert.equal(resultado, 'Grace',`consultarTabla debería retornar 'Grace' pero fue ${resultado}`);
+})
+
+it(`Si hacemos consultarTabla(1, 'email', tabla) debería retornar 'Grace'`, function() {
+  let resultado = consultarTabla(1, 'email', [
+  ['id', 'nombre', 'edad', 'email'],
+  [1, 'Ada', 33, 'ada@gmail.com'],
+  [2, 'Grace', 45, 'grace@gmail.com'],
+])
+  assert.equal(resultado, 'ada@gmail.com',`consultarTabla debería retornar 'ada@gmail.com' pero fue ${resultado}`);
 })
