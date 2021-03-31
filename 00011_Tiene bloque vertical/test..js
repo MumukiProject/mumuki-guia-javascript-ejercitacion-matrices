ce658cc66661f8b05d2cfcc1ffbe5440c7558776
franco@mumuki.org <franco@mumuki.org>
2021-03-31 20:32:42 +0000

    Mumuki Export on 2021-03-31 20:32:42 +0000

--- 00011_Tiene bloque vertical/test..js
+++ 00011_Tiene bloque vertical/test..js
@@ -1,12 +1,12 @@
 it(`Si hacemos tieneBloqueVertical([
   [4, 2, 3],
   [1, 5, 3],
-  [1, 2, 3],
+  [1, 2, 3]
 ]) debería retornar true`, function() {
   let resultado = tieneBloqueVertical([
   [4, 2, 3],
   [1, 5, 3],
-  [1, 2, 3],
+  [1, 2, 3]
 ])
   assert.equal(resultado, true,`tieneBloqueVertical debería retornar true pero fue ${resultado}`);
 })
@@ -14,7 +14,7 @@ it(`Si hacemos tieneBloqueVertical([
 it(`Si hacemos tieneBloqueVertical([
   [1, 2, 3, 4],
   [7, 2, 9, 2],
-  [1, 8, 4, 5],
+  [1, 8, 4, 5]
 ])  debería retornar false`, function() {
   let resultado = tieneBloqueVertical([
   [1, 2, 3, 4],
@@ -31,7 +31,7 @@ it(`Si hacemos tieneBloqueVertical([
   [4, 5],
   [1, 5],
   [1, 4],
-  [4, 5],
+  [4, 5]
 ])  debería retornar false`, function() {
   let resultado = tieneBloqueVertical([
   [3, 4],
@@ -39,7 +39,7 @@ it(`Si hacemos tieneBloqueVertical([
   [4, 5],
   [1, 5],
   [1, 4],
-  [4, 5],
+  [4, 5]
 ]) 
   assert.equal(resultado, false,`tieneBloqueVertical debería retornar false pero fue ${resultado}`);
 })
