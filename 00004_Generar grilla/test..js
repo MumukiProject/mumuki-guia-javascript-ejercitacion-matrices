64e02942d706fe98c4de6319c8e085701beac2a6
gustavo@mumuki.org <gustavo@mumuki.org>
2021-04-07 16:03:35 +0000

    Mumuki Export on 2021-04-07 16:03:35 +0000

--- 00004_Generar grilla/test..js
+++ 00004_Generar grilla/test..js
@@ -1,3 +1,30 @@
+it(`Si hacemos generarGrilla(2, 3, [1, 2]) debería retornar una matriz con 2 filas`, function(){
+  let resultado = generarGrilla(2, 3, [1, 2])
+  assert.equal(resultado.length, 2),`generarGrilla debería retornar una matriz con 2 filas pero fue ${resultado.length}`);
+})
+
+it(`Si hacemos generarGrilla(2, 3, [1, 2]) debería retornar una matriz con 3 columnas`, function(){
+  let resultado = generarGrilla(2, 3, [1, 2])
+  assert.equal(resultado[0].length,3),`generarGrilla debería retornar una matriz con 3 columnas pero fue ${resultado[0].length}`);
+})
+
+it(`Si hacemos generarGrilla(6, 5, [2, 4, 8]) debería retornar una matriz con 6 filas`, function(){
+  let resultado = generarGrilla(6, 5, [2, 4, 8])
+  assert.equal(resultado.length, 6),`generarGrilla debería retornar una matriz con 6 filas pero fue ${resultado.length}`);
+})
+
+it(`Si hacemos generarGrilla(6, 5, [2, 4, 8]) debería retornar una matriz con 5 columnas`, function(){
+  let resultado = generarGrilla(6, 5, [2, 4, 8])
+  assert.equal(resultado[0].length,5),`generarGrilla debería retornar una matriz con 5 columnas pero fue ${resultado[0].length}`);
+})
+
+it(`Si hacemos generarGrilla(8, 7, [2, 5, 7]) 2 veces seguidas debería retornas matrices distintas`, function(){
+  let resultado1 = generarGrillagenerarGrilla(8, 7, [2, 5, 7]);
+  let resultado2 = generarGrillagenerarGrilla(8, 7, [2, 5, 7]);
+  assert.notDeepEqual(resultado1, resultado2);
+})
+
+/*
 it(`Si hacemos generarGrilla(2, 3, [1, 2]) debería retornar [
     [1, 1, 2], 
     [2, 1, 1]
@@ -27,4 +54,4 @@ it(`Si hacemos generarGrilla(3, 3, ['a', 'b', 'c']) debería retornar [
     ['c', 'a', 'a'], 
     ['b', 'a', 'b']
   ] pero fue ${resultado}`);
-})
+})*/
